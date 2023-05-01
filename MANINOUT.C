ae219b390922bb54fbc3edcfcc8bbefe4db94415
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-05-01 15:09:09 +0530

    Add files via upload

---
+++ MANINOUT.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c;
+	clrscr();
+	printf("enter value a=");
+	scanf("%d",&b);
+	printf("enter value b=");
+	scanf("%d",&a);
+	printf("enter value c=");
+	scanf("%d",&c);
+	printf("additon of a=%d",a);
+	printf("additon of b=%d",b);
+	getch();
+}
